b7349a7e7587ecb87aea025de3e7152aaf49d5f3
Dariusz <d.kuriata88@gmail.com>
2021-04-28 17:13:38 +0200

    Driver init and first test

---
+++ Kurs Selenium/UnitTest1.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Kurs_Selenium
+{
+    public class Tests
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Navigate().GoToUrl("http://google.pl");
+        }
+    }
+}
